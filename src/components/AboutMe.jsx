61c3df782d349c23cb7f3c42088df77c151eb074
lgonzalez9 <lgonzalez@midasconsultores.com.ar>
2021-12-14 22:19:13 -0300

    Inicio y menu

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <section id="sobre-mi" class="contenedor">
+            <h2>Sobre Mí</h2>
+            <p>Te cuento un poco sobre mí. Actualmente soy desarrollador web frontend (React JS) en Midas Consultores, estudiante de la tecnicatura superior en programación (UTN), y autodidacta, estoy en busca de ganar cada vez más experiencia. Tengo conocimientos en diferentes tecnologías, especializandome en el desarrollo web. Poseo un nivel de inglés intermedio (No conversacional), responsable, proactivo, con capacidad de trabajar en equipo, de adaptarme al cambio y aprender rápidamente, y con muchas ganas de expandir mis conocimientos en las tecnologías actuales.</p>
+        </section>
+    );
+}
+ 
+export default AboutMe;
